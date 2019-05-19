9c3c31c87d802cb796915c5e30508da680bc2a79
pantao <tao@zhazhapan.com>
2019-05-19 21:59:46 +0800

    生成器与迭代器

--- demo-basis/advanced_feature.py
+++ demo-basis/advanced_feature.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 
 from collections.abc import Iterable
+from collections.abc import Iterator
 import os
 
 print('\n切片\n')
@@ -62,3 +63,39 @@ print([x * x for x in range(1, 11) if x % 2 == 0])
 print([m + n for m in 'abc' for n in 'xyz'])
 print([d for d in os.listdir('.')])
 print([k + '=' + str(v) for k, v in kv.items()])
+
+# -----------------------生成器---------------------
+
+print('\n生成器\n')
+
+g = (x * x for x in range(1, 10))
+
+print(next(g))
+print(next(g))
+
+for n in g:
+    print(n)
+
+
+def fib(lens):
+    n, a, b = 0, 0, 1
+    while n < lens:
+        yield b
+        n += 1
+        a, b = b, a + b
+    return 'done'
+
+
+# ---------------------迭代器-------------------
+
+print('\n迭代器\n')
+
+fib_res = fib(10)
+if isinstance(fib_res, Iterator):
+    for n in fib(10):
+        print(n)
+
+print()
+
+for n in iter(li):
+    print(n)
