1eeb9297fe941c2c56e4be54d07f0b9d7c4f79ea
pantao <easepan@qq.com>
2019-07-01 15:28:52 +0800

    feat: do leetcode '最长回文串'

--- demo-algorithm/strings/solution.py
+++ demo-algorithm/strings/solution.py
@@ -5,6 +5,25 @@ from collections import Counter
 
 class Solution:
 
+    def longestPalindrome(self, s: str) -> int:
+        """
+        LeetCode(id=409,title=最长回文串,difficulty=medium)
+        """
+        m = {}
+        for c in s:
+            m[c] = m.get(c, 0) + 1
+        size = 0
+        add = False
+        for v in m.values():
+            if v & 1 == 1:
+                add = True
+                size = size + v - 1
+            else:
+                size = size + v
+        if add:
+            size = size + 1
+        return size
+
     def frequency_sort(self, s: str) -> str:
         """
         LeetCode(id=451,title=根据字符出现频率排序,difficulty=medium)
